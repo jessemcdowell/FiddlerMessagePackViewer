d07ed2fd5162350b2b977e8f90ea0b0e34906db4
jessemcdowell <jessemcd1@gmail.com>
2015-05-31 20:59:34 -0700

    switch to run in fiddler2

--- MsgPackViewer/Properties/AssemblyInfo.cs
+++ MsgPackViewer/Properties/AssemblyInfo.cs
@@ -15,4 +15,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
-[assembly: Fiddler.RequiredVersion("4.5.0.0")]
+[assembly: Fiddler.RequiredVersion("2.5.0.0")]
